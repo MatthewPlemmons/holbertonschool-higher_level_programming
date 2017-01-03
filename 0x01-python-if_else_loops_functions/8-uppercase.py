b7e3902ac00b2d44ac8ab8b6300dd90414ccaba7
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-01-03 22:26:58 +0000

    removed print function

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,7 @@
+#!/usr/bin/python3
 def uppercase(str):
     for c in str:
         if ord(c) in range(97, 123):
-            c = ord(c) - 32
-            print(chr(c), end="")
-        else:
-            print(c, end="")
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
     print("")
