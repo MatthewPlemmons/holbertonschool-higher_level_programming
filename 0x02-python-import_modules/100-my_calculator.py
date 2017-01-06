f2cecf611ed25b9479f0a8b72eb9b2f92ec9b20c
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-01-06 04:43:00 +0000

    task 6 solution

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+from sys import argv
+from calculator_1 import add, sub, mul, div
+
+import pdb
+
+def main():
+
+    pdb.set_trace()
+
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py a operator b")
+        exit(1)
+
+    a = int(argv[1])
+    b = int(argv[3])
+    op = argv[2]
+
+    op_dict = {"+": add, "-": sub, "*": mul, "/": div}
+    if str(op) not in op_dict:
+        print("Unkown operator.  Available operators: +, -, * and /")
+        exit(1)
+
+    print("{:d} {} {:d} = {:d}".format(a, op, b, op_dict[op](a, b)))
+
+    """if op == "+":
+        n = add(a, b)
+        print(n)
+    if op == "-":
+        print(sub(a, b))
+    if op == "*":
+        print(mul(a, b))
+    if op == "/":
+        print(div(a, b))
+    """
+    exit(0)
+
+if __name__ == "__main__":
+    main()
