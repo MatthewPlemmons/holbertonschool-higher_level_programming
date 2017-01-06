2c17d730c15d1ec20afae042e734f4d87a57e7bb
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-01-06 01:20:43 +0000

    script prints inputted arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 from sys import argv
 
-n = len(argv) - 1
-
-print("{} argument{}".format(n, "s:" if n > 1 else ":" if n == 1 else "."))
-for num, arg in enumerate(argv[1:]):
-    print("{}: {}".format(num + 1, arg))
+if __name__ == "__main__":
+    n = len(argv) - 1
+    print("{} argument{}".format(n, "s:" if n > 1 else ":" if n == 1 else "."))
+    for num, arg in enumerate(argv[1:]):
+        print("{}: {}".format(num + 1, arg))
