8065b277872e4de0c167fbb67364859ef3bd2d48
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-01-06 02:23:02 +0000

    edit script

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import hidden_4
 
-for name in dir(hidden_4):
-    if name[:2] != "__":
-        print(name)
+names = dir(hidden_4)
+for name in names:
+    if "__" not in name:
+        print("{}".format(name))
