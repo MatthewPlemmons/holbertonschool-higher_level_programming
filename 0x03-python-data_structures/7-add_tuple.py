67ec4d9d21785fa99446711ef54326f6de96d919
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-01-11 05:45:44 +0000

    breaks after first 2 iterations

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,16 +5,16 @@ def add_tuple(tuple_a=(), tuple_b=()):
     a = [0, 0]
     b = [0, 0]
     n = 0
-    for i in range(len(tuple_a)):
+    for i in range(len(tuple_a):
+        a[i] += tuple_a[i]
         if n < 2:
             break
-        a[i] += tuple_a[i]
         n += 1
     n = 0
     for i in range(len(tuple_b)):
+        b[i] += tuple_b[i]
         if n < 2:
             break
-        b[i] += tuple_b[i]
         n += 1
     tuple_c = (a[0] + b[0], a[1] + b[1])
     return (tuple_c)
