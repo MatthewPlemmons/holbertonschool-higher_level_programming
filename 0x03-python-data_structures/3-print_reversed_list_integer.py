dfff5e88b3e02bc6399d46d6361d42c6d69e6a44
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-01-11 00:02:59 +0000

    edited code for checker

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,8 +2,6 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    if not my_list:
-        return (my_list)
     my_list.reverse()
     for i in my_list:
-        print("{}".format(i))
+        print("{:d}".format(i))
