5ff7cd9f8b9dc7a7600bf347928dc0d7be097abf
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-01-11 05:46:36 +0000

    edits

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,17 +4,9 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     a = [0, 0]
     b = [0, 0]
-    n = 0
     for i in range(len(tuple_a):
         a[i] += tuple_a[i]
-        if n < 2:
-            break
-        n += 1
-    n = 0
     for i in range(len(tuple_b)):
         b[i] += tuple_b[i]
-        if n < 2:
-            break
-        n += 1
     tuple_c = (a[0] + b[0], a[1] + b[1])
     return (tuple_c)
