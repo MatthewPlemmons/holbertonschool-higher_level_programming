823e97c1560d8af9ad82d189f1e95da887e67cf8
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-03-05 19:37:05 +0000

    python script to list database entries

---
+++ 0x0E-python-object_relational_mapping/0-selected_sates.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Lists states from the database hbtn_0e_0_usa"""
+
+from sys import argv as a
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=a[1], password=a[2], database=a[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states")
+    for n in cursor.fetchall():
+        print(n)
