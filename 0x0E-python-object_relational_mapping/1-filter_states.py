67bf6bf79867140a237f41796a66dee881479be5
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-03-05 19:59:55 +0000

    add task 1 script

---
+++ 0x0E-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Lists states from the database hbtn_0e_0_usa"""
+
+from sys import argv as a
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=a[1], password=a[2], database=a[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states")
+    for n in cursor.fetchall():
+        if n[1][0] == "N":
+            print(n)
+
+    cursor.close()
+    db.close()
