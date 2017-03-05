f3cde86e339dcac7fd66b0c78a0c66d3a26e1b45
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-03-05 20:11:48 +0000

    script to return user specified row

---
+++ 0x0E-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Lists states from the database hbtn_0e_0_usa"""
+
+from sys import argv as a
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=a[1], passwd=a[2], db=a[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states")
+    for n in cursor.fetchall():
+        if a[4] == n[1]:
+            print(n)
+
+    cursor.close()
+    db.close()
