29b27f1e29509e3d363bb08f05334258a4ee93ef
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-03-06 09:05:09 +0000

    edited state class

--- 0x0E-python-object_relational_mapping/model_state.py
+++ 0x0E-python-object_relational_mapping/model_state.py
@@ -3,10 +3,10 @@ import sqlalchemy
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
 
 """Model of state table"""
 
-Base = declarative_base()
 
 class State(Base):
     __tablename__ = "states"
