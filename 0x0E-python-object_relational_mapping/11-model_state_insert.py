4eb229a6da588250803d2ab5dcc821dd30eb0b2c
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-03-06 10:14:26 +0000

    script to add state

---
+++ 0x0E-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Lists states that contain the letter a from the database hbtn_0e_6_usa"""
+
+from sys import argv as a
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+
+
+if __name__ == "__main__":
+    Session = sessionmaker()
+    engine = create_engine("mysql://{}:{}@localhost:3306/{}"
+                           .format(a[1], a[2], a[3]))
+    Session.configure(bind=engine)
+    sess = Session()
+    state = State(name="Louisiana")
+    sess.add(state)
+    sess.commit()
+    print(state.id)
