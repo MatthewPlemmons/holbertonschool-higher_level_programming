158231244f78a712b71c322ee9ab5e228a3d7195
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-03-06 09:02:21 +0000

    edited state class

--- 0x0E-python-object_relational_mapping/model_state.py
+++ 0x0E-python-object_relational_mapping/model_state.py
@@ -10,5 +10,5 @@ Base = declarative_base()
 
 class State(Base):
     __tablename__ = "states"
-    id = Column(Integer, unique=True, autoincrement=True, nullable=False, primary_key=True)
+    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
