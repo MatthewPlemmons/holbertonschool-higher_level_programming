91c037d82f2586d48339ac1b0fd1fbd6f7a8cef8
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-03-06 04:50:32 +0000

    script to show city and state from database

---
+++ 0x0E-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Lists states from the database hbtn_0e_0_usa"""
+
+from sys import argv as a
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=a[1], passwd=a[2], db=a[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT cities.id, cities.name, states.name \
+    FROM cities JOIN states WHERE state_id = states.id;")
+    for n in cursor.fetchall():
+            print(n)
+
+    cursor.close()
+    db.close()
