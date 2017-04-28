cf445b8b239bcb8674b5c2a9471af3a5ec813080
MatthewPlemmons <Matthewleeplemmons@gmail.com>
2017-04-28 04:55:56 +0000

    fix formatting

--- 0x11-javascript-warm_up/10-factorial.js
+++ 0x11-javascript-warm_up/10-factorial.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 function factorial (a) {
   if (a > 0) {
-    return(a * factorial(--a));
+    return (a * factorial(--a));
   } else {
-    return(1);
+    return (1);
   }
 }
 let n = parseInt(process.argv[2]);
