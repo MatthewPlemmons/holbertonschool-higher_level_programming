9138e2a7d4ba7d4f605f1f35c865277a377ce0a8
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-04-30 11:32:12 -0500

    remove semicolon

--- 0x12-javascript-web_scraping/5-square.js
+++ 0x12-javascript-web_scraping/5-square.js
@@ -2,7 +2,7 @@
 const Rectangle = require('./4-rectangle').Rectangle;
 function Square (size) {
   Rectangle.call(this, size, size);
-};
+}
 Square.prototype = Object.create(Rectangle.prototype);
 Square.prototype.constructor = Square;
 exports.Square = Square;
