1e217efb9badbc5a991dcbf7d49f9ba206b8c373
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-04-30 13:24:24 -0500

    add task 12 file

---
+++ 0x12-javascript-web_scraping/12-request_store.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+request(process.argv[2], (error, response, body) => {
+  if (error) return console.log(error);
+  fs.writeFile(process.argv[3], body, (error) => {
+    if (error) return console.log(error);
+  });
+});
