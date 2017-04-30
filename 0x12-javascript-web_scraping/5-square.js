9e032f916a77fb175c6ef26a12dc76a7c1647e8d
MatthewPlemmons <matthewleeplemmons@gmail.com>
2017-04-30 11:31:05 -0500

    edit

--- 0x12-javascript-web_scraping/5-square.js
+++ 0x12-javascript-web_scraping/5-square.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 const Rectangle = require('./4-rectangle').Rectangle;
-exports.Square = function Square (size) {
+function Square (size) {
   Rectangle.call(this, size, size);
-  Square.prototype = Object.create(Rectangle.prototype);
-  Square.prototype.constructor = Square;
-}
+};
+Square.prototype = Object.create(Rectangle.prototype);
+Square.prototype.constructor = Square;
+exports.Square = Square;
